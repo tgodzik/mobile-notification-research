fd9033c6d27fab6d0bddb5410b40e20c363495fa
Mikołaj Jakubowski <mkl.jakubowski@gmail.com>
2014-06-15 20:28:56 +0200

    python xmpp message sender

---
+++ xmpp-test/spam.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+import getpass
+import time
+import sleekxmpp
+
+class SendMsgBot(sleekxmpp.ClientXMPP):
+
+    def __init__(self, jid, password, recipient):
+        sleekxmpp.ClientXMPP.__init__(self, jid, password)
+
+        self.recipient = recipient
+        self.add_event_handler("session_start", self.start, threaded=True)
+
+    def start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+        while True:
+            self.send_message(mto=self.recipient, mbody=str(int(round(time.time() * 1000))), mtype='chat')
+
+        self.disconnect(wait=True)
+
+
+if __name__ == '__main__':
+    xmpp = SendMsgBot('worker@192.168.2.10', 'ala123', 'admin@192.168.2.10')
+    xmpp.register_plugin('xep_0030') # Service Discovery
+    xmpp.register_plugin('xep_0199') # XMPP Ping
+
+    if xmpp.connect():
+        xmpp.process(block=True)
+        print("Done")
+    else:
+        print("Unable to connect.")
