2a14697053cf1168807288f8dd98096ebca77792
Mikołaj Jakubowski <mjakubowski@virtuslab.com>
2015-11-20 12:21:44 +0100

    modbus speed test

--- modbus-test/modbus-server/src/main/scala/pl/edu/agh/mobilne/modbus_test/app/ModbusServer.scala
+++ modbus-test/modbus-server/src/main/scala/pl/edu/agh/mobilne/modbus_test/app/ModbusServer.scala
@@ -1,6 +1,7 @@
 package pl.edu.agh.mobilne.modbus_test.app
 
 import java.net.InetAddress
+import java.util.Date
 
 import com.ghgande.j2mod.modbus.io.ModbusTCPTransaction
 import com.ghgande.j2mod.modbus.msg.ReadMultipleRegistersRequest
@@ -12,13 +13,23 @@ object ModbusServer extends App {
   val con = new TCPMasterConnection(addr)
   con.setPort(port)
   con.connect()
+  val count = 100
+  val noOfMsg = 100
 
-  (0 until 100).foreach{ i =>
-    val req = new ReadMultipleRegistersRequest(1, 1)
-    val trans = new ModbusTCPTransaction(con)
-    trans.setRequest(req)
-    trans.execute()
-    println(trans.getResponse.getHexMessage.substring(27))
-  }
+  val avg = (0 until count).map { x =>
+    val beginTime = new Date().getTime
+    (0 until noOfMsg).foreach { i =>
+      val req = new ReadMultipleRegistersRequest(1, 1)
+      val trans = new ModbusTCPTransaction(con)
+      trans.setRequest(req)
+      trans.execute()
+      //    println(trans.getResponse.getHexMessage.substring(27))
+    }
+    val duration = new Date().getTime - beginTime
+    println(duration)
+    duration
+  }.sum / count
+
+  println(s"avg $avg")
   con.close()
 }
